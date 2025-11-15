d6248f202d36ada9d71db54f4dfc36e1312a2f0d
vera <vis9391@students.fhv.at>
2025-11-15 20:07:07 +0100

    player sprite

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,54 +1,54 @@
 extends CharacterBody2D
 
+@onready var anim = $AnimatedSprite2D
+
 const GRAVITY := 900.0
 const SPEED := 120.0
-const JUMP_FORCE := -350.0
+const JUMP_FORCE := -450.0
+const MAX_SPEED_MULTIPLIER := 2.5  
 
 var speed_multiplier := 1.0
 
 # --- COYOTE TIME ---
-var coyote_time := 0.1
+var coyote_time := 0.2
 var coyote_timer := 0.0
 
 # --- DOUBLE JUMP ---
 var max_jumps := 2
 var jumps_left := 2
+
 func _physics_process(delta):
-	
 	if position.y > 900:
 		get_tree().change_scene_to_file("res://Scenes/game_over.tscn")
 
 	# --- COYOTE TIME ---
 	if is_on_floor():
 		coyote_timer = coyote_time
-		jumps_left = max_jumps       # Sprünge zurücksetzen
+		jumps_left = max_jumps
 	else:
 		coyote_timer -= delta
 
 	# --- LINKS ---
 	if Input.is_action_pressed("left"):
 		velocity.x = -SPEED * speed_multiplier
-		$Sprite2D.flip_h = false
+		anim.flip_h = true   # LINKS
 
 	# --- RECHTS ---
 	elif Input.is_action_pressed("right"):
 		velocity.x = SPEED * speed_multiplier
-		$Sprite2D.flip_h = true
+		anim.flip_h = false  # RECHTS
 
 	# --- IDLE ---
 	else:
 		velocity.x = 0
 
-	# --- JUMP (auch Double Jump) ---
+	# --- JUMP + DOUBLE JUMP ---
 	if Input.is_action_just_pressed("jump"):
-		
-		# normaler Sprung oder Sprung an Kante
 		if coyote_timer > 0 or is_on_floor():
 			velocity.y = JUMP_FORCE
 			jumps_left -= 1
 			coyote_timer = 0
 
-		# DOUBLE JUMP
 		elif jumps_left > 0:
 			velocity.y = JUMP_FORCE
 			jumps_left -= 1
@@ -60,5 +60,20 @@ func _physics_process(delta):
 	# --- MOVE ---
 	move_and_slide()
 
-	# --- SPEED WIRD IMMER SCHNELLER ---
-	speed_multiplier += 0.05 * delta
+	
+	speed_multiplier = min(speed_multiplier + 0.05 * delta, MAX_SPEED_MULTIPLIER)  
+
+	# --- ANIMATION ---
+	update_animation()
+	
+
+func update_animation():
+	if not is_on_floor():
+		if velocity.y < 0:
+			anim.play("jump")
+		return
+
+	if velocity.x == 0:
+		anim.play("idle")
+	else:
+		anim.play("run")
